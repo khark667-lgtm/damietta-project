9911db0e138955bf3702502cbe0c8ff2c8a8fdd4
khark667-lgtm <khark667@gmail.com>
2025-09-12 21:55:19 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('.header');
-
-window.addEventListener('scroll', () => {
-  if(window.scrollY > 10) {  // لو المستخدم بدأ النزول
-    header.classList.add('scrolled');
-  } else {                   // رجع لفوق الصفحة
-    header.classList.remove('scrolled');
-  }
-});
